5210a6316481ee924b0d12106fd9eb5e69f1f342
Pintta <69728770+Pintta@users.noreply.github.com>
2023-07-27 10:31:30 +0300

    Add files via upload

---
+++ qb-hud/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+shared_script 'config.lua'
+client_script 'client.lua'
+server_script 'server.lua'
+ui_page 'html/index.html'
+files {
+    'html/*',
+    'html/index.html',
+    'html/styles.css',
+    'html/responsive.css',
+    'html/app.js',
+}
